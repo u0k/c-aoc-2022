6852bd4f1f74dae96d0cf1fa9bfebc790d0d5f19
u0k <dedmukas@gmail.com>
2023-08-30 17:25:46 +0300

    completed first day

--- first_week/day1/calorie_count.c
+++ first_week/day1/calorie_count.c
@@ -5,6 +5,7 @@ int main(int argc, char *argv[]) {
 
   FILE *fp = fopen(argv[1], "r");
   char chbf[128];
+  u_int32_t top_sums[3] = {0, 0, 0};
   if (fp == NULL) {
     printf("%s", "FILE NOT FOUND");
     return EXIT_FAILURE;
@@ -12,6 +13,13 @@ int main(int argc, char *argv[]) {
   u_int32_t sum = 0, max = 0;
   while (fgets(chbf, sizeof(chbf), fp) != NULL) {
     if (chbf[0] == '\n') {
+      if (sum > top_sums[0]) {
+        top_sums[0] = sum;
+      } else if (sum > top_sums[1]) {
+        top_sums[1] = sum;
+      } else if (sum > top_sums[2]) {
+        top_sums[2] = sum;
+      }
       if (sum > max) {
         max = sum;
       }
@@ -20,6 +28,7 @@ int main(int argc, char *argv[]) {
     sum += atoi(chbf);
   }
   printf("%d\n", max);
+  printf("%d\n", top_sums[0] + top_sums[1] + top_sums[2]);
 
   return EXIT_SUCCESS;
 }
